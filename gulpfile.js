24bb34c3a10c582b16529fb6265ce9ca0b1c0017
Heath Stewart <heaths@outlook.com>
2016-01-30 03:29:28 -0800

    Revert back to SVG and automate build

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+// The MIT License (MIT)
+//
+// Copyright (c) Heath Stewart
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var merge = require('gulp-merge');
+var mocha = require('gulp-mocha');
+var svgmin = require('gulp-svgmin');
+var ts = require('gulp-typescript');
+
+gulp.task('compile', function() {
+    var project = ts.createProject('tsconfig.json');
+    return merge(
+        gulp.src('res/**/*.svg')
+            .pipe(svgmin())
+            .pipe(gulp.dest('out/res/')),
+        project.src()
+            .pipe(ts(project))
+            .js.pipe(gulp.dest('out'))
+    );
+});
+
+gulp.task('watch', ['compile'], function() {
+    gulp.watch(['src/**/*.ts', 'test/**/*.ts'], ['compile']);
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['out', '**/*.vsix'])
+        .pipe(clean({read: false}));
+});
+
+gulp.task('test', ['compile'], function() {
+    return gulp.src('out/test/**/*.js')
+        .pipe(mocha({ui: 'tdd'}));
+});
+
+gulp.task('default', ['test']);
